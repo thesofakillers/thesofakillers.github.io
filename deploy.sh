756b151b1d65206bd34105c0f326615826fb6a8d
Giulio Starace <giulio.starace@gmail.com>
2020-12-31 18:39:33 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+  msg="$*"
 fi
 git commit -m "$msg"
 
